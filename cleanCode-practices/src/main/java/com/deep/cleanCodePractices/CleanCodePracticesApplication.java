0cabf37decc05c9ee43f17a50ae9bfe46116ebac
deepak <dipakpatilcse@gmail.com>
2020-10-12 03:29:09 +0530

    Clean code practices

---
+++ cleanCode-practices/src/main/java/com/deep/cleanCodePractices/CleanCodePracticesApplication.java
@@ -0,0 +1,13 @@
+package com.deep.cleanCodePractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodePracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodePracticesApplication.class, args);
+	}
+
+}
---
+++ cleanCode-practices/src/test/java/com/deep/cleanCodePractices/CleanCodePracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.cleanCodePractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodePracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
